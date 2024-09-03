8b3efa4396a1748aec9e2fe42261e3161c015890
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-03 12:34:58 +0800

    Description of the changes

--- client/script.js
+++ client/script.js
@@ -4,3 +4,19 @@ import user from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
+
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
